5ab0726c7fc679ae0bcef36f3d38c6080eb3c83f
Jannis R <mail@jannisr.de>
2016-12-17 01:47:51 +0100

    tests for places :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
 'use strict'
 
 const assert = require('assert')
-const routes = require('./index').routes
+const {routes, places} = require('.')
 
 routes('FRLPD', 'DEFRA')
 .then((data) => assert.strictEqual(typeof data, 'object', 'Response is not an object'))
+.catch(assert.ifError)
+
+places('Lyon')
+.then((results) => {
+	assert.ok(Array.isArray(results), 'Response is not an array.')
+	for (let result of results) {
+		assert.strictEqual(typeof result.name, 'string', 'Name in result is not a string.')
+		assert.strictEqual(typeof result.id, 'string', 'ID in result is not a string.')
+	}
+})
 .catch(assert.ifError)
